48b49a9ef709c7c39e337bd3379f6c6c07fa746c
baseer ahmed <ahmedbaserr75@gmail.com>
2024-02-02 11:36:40 +0500

    fix type interface

--- src/apis/insightApi.ts
+++ src/apis/insightApi.ts
@@ -1,35 +1,36 @@
-import axios, {AxiosRequestConfig} from 'axios';
-import {api, setBaseUrl} from './apiConstants';
+import axios, { AxiosRequestConfig } from 'axios';
+import { api, setBaseUrl } from './apiConstants';
 
-
-export const getGlucoseInsights = async (accessToken: string,): Promise<InsightCard[]> => {
-    
-    const options: AxiosRequestConfig = {
-      url: `${api.users.glucoseInsights}`, 
-      method: 'GET',
-      headers: {
-        Authorization: `Bearer ${accessToken}`,
-      },
-    };
-  
-    try {
-      const result = await axios.request(options);
-      return result.data?.insightCards as InsightCard[];
-    } catch (error: any) {
-      // Handle errors here
-      console.error('Error:', error.message);
-      return Promise.reject(error);
-    }
+export const getGlucoseInsights = async (accessToken: string): Promise<InsightCard[]> => {
+  const options: AxiosRequestConfig = {
+    url: `${api.users.glucoseInsights}`,
+    method: 'GET',
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+    },
   };
 
-
-//-- Modal --//
-interface InsightCard {
-    type: string;
-    graph?: {
-      type: string;
-      value: number;
-    };
-    title: string;
-    description: string;
+  try {
+    const result = await axios.request(options);
+    return result.data.insightCards as InsightCard[];
+  } catch (error: any) {
+    // Handle errors here
+    console.error('Error:', error.message);
+    return Promise.reject(error);
   }
+};
+
+//---//
+type InsightCardGraph = {
+  type: 'PROGRESS_CIRCLE';
+  value: number;
+};
+
+type InsightCard = {
+  type: 'OVERVIEW' | 'INSIGHT_CARD';
+  graph?: InsightCardGraph;
+  title: string;
+  description: string;
+  image?: string;
+  link?: string;
+};
